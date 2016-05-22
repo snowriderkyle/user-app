a0363419731a5c6011a35626a6dafe7a35bd0c6e
Kyle van Til <snowrider_kyle@hotmail.com>
2016-05-22 16:16:57 +0200

    Completed part 2

--- Public/script.js
+++ Public/script.js
@@ -1,12 +1,19 @@
 var MIN_LENGTH = 1;
 $( document ).ready(function() {
 	console.log("Dom is ready")
+	
+var fireRequest = true;
+
 	$("input").keyup(function() {
 		console.log('typing');
 		var postdata = {
-			search: $(this).val()
+			search: $(this).val(), ajax : true
 		}
 		console.log(postdata);
+if( postdata.search ){ 
+	if( fireRequest){
+		fireRequest = false
+	
 
 		$.post('/Api', postdata, function(data){
 			$ ('#displaylist').empty()
@@ -16,6 +23,12 @@ $( document ).ready(function() {
 				$ ( '#displaylist' ).append ('<option>' + data[person])
 			} 
 		});
+		setTimeout(function() {
+    			fireRequest = true
+			}, 300);
+	}
+}
+
 });
 });
 // 
